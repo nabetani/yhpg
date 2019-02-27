319782bbc0adaa4994475fc5fd655274dd42862c
Nabetani <takenori@nabetani.sakura.ne.jp>
2019-02-27 22:17:04 +0900

    解く

--- e31/src/solver.rb
+++ e31/src/solver.rb
@@ -2,6 +2,73 @@
 
 require "pp"
 
+DIGITS=[*"0".."9", *"a".."z"]
+
+def number(r)
+  r.map{ |e| DIGITS[e] }.join
+end
+
+def build_number(b, head, size, states)
+  r=[head]
+  (size-1).times do |ix|
+    p = r.last
+    candidates = [ p, (p+1) % b ].sort
+    r.push case states[ix]
+    when nil
+      candidates[0]
+    when :below
+      candidates[0]
+    when :low
+      candidates[0]
+    when :mid
+      candidates[1]
+    when :high
+      candidates[1]
+    when :above
+      raise( "unexpected" )
+    end
+  end
+  number(r)
+end
+
+def lowest_guru(b,len,head)
+  if head==b
+    "1"*(len+1)
+  else
+    DIGITS[head+1]*len
+  end
+end
+
+def solve_impl(b,n)
+  n+=1
+  digits = n.digits(b).reverse
+  ignore=false
+  states = digits.each_cons(2).map{ |p,q|
+    candidates = [ p, (p+1) % b ].sort
+    if ignore
+      nil
+    elsif q<candidates[0]
+      ignore=true
+      :below
+    elsif q==candidates[0]
+      :low
+    elsif q<candidates[1]
+      ignore=true
+      :mid
+    elsif q==candidates[1]
+      :high
+    else
+      ignore=true
+      :above
+    end
+  }
+  if states.include?(:above)
+    lowest_guru(b, digits.size, digits.first)
+  else
+    build_number(b, digits.first, digits.size, states)
+  end
+end
+
 def solve( src )
-  "NOT_IMPL:#{src}"
+  solve_impl(*src.split(",").map(&:to_i))
 end
