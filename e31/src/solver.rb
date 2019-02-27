215efe89d42648fee48f8b56ffe8e4fc0a0ff032
Nabetani <takenori@nabetani.sakura.ne.jp>
2019-02-27 22:57:57 +0900

    計算間違いが合ったので修正する

--- e31/src/solver.rb
+++ e31/src/solver.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require "pp"
-
 DIGITS=[*"0".."9", *"a".."z"]
 
 def number(r)
@@ -13,35 +11,23 @@ def build_number(b, head, size, states)
   (size-1).times do |ix|
     p = r.last
     candidates = [ p, (p+1) % b ].sort
-    r.push case states[ix]
-    when nil
-      candidates[0]
-    when :below
-      candidates[0]
-    when :low
-      candidates[0]
-    when :mid
-      candidates[1]
-    when :high
-      candidates[1]
-    when :above
-      raise( "unexpected" )
-    end
+    r.push candidates[
+      [nil, :below, :low].include?(states[ix]) ? 0 : 1
+    ]
   end
-  number(r)
+  r
 end
 
-def lowest_guru(b,len,head)
-  if head==b
-    "1"*(len+1)
-  else
-    DIGITS[head+1]*len
+def undigits(b,d)
+  r=0
+  d.reverse.each do |n|
+    r*=b
+    r+=n
   end
+  r
 end
 
-def solve_impl(b,n)
-  n+=1
-  digits = n.digits(b).reverse
+def solve_impl(b,digits)
   ignore=false
   states = digits.each_cons(2).map{ |p,q|
     candidates = [ p, (p+1) % b ].sort
@@ -62,13 +48,29 @@ def solve_impl(b,n)
       :above
     end
   }
-  if states.include?(:above)
-    lowest_guru(b, digits.size, digits.first)
+  above = states.index(:above)
+  if above
+    n = undigits(b,digits[0,above+1].reverse)
+    d = (n+1).digits(b).reverse
+    head = solve_impl(b,d)
+    num = [ head.last, (head.last+1) % b ].min
+    tail = [num] * (digits.size-above-1)
+    head+tail
   else
     build_number(b, digits.first, digits.size, states)
   end
 end
 
 def solve( src )
-  solve_impl(*src.split(",").map(&:to_i))
+  b,n = src.split(",").map(&:to_i)
+  digits = (n+1).digits(b).reverse
+  solve_impl(b, digits).map{ |e| DIGITS[e] }.join
+end
+
+if __FILE__==$PROGRAM_NAME
+  [ "4,828" ].each do |src|
+    b,n = src.split(",").map(&:to_i)
+    digit = n.to_s(b)
+    puts( "%s ( %s ) -> %s" % [src, digit, solve(src)] )
+  end
 end
