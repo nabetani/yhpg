b2beab7dbc3ca2aa58826bc2eb22c594757540a1
Nabetani <takenori@nabetani.sakura.ne.jp>
2019-02-27 23:04:13 +0900

    実行時間を出力する

--- e31/src/solver_test.rb
+++ e31/src/solver_test.rb
@@ -1,15 +1,27 @@
 # frozen_string_literal: true
 
 require 'minitest/autorun'
+require 'benchmark'
 require_relative "solver"
 require_relative "solve_slow"
 require_relative "testcase"
 
 class SolverTest < Minitest::Test
+  def setup
+    @slow_ticks = 0
+    @prod_ticks = 0
+  end
+
+  def teardown
+    puts( "slow: %g[sec], prod: %g[sec]" % [ @slow_ticks, @prod_ticks ] )
+  end
+
   def test_samples
     TESTCASES.each do |src|
-      expected = solve_slow(src)
-      actual = solve( src )
+      expected = nil
+      actual = nil
+      @slow_ticks += Benchmark.realtime{ expected = solve_slow(src) }
+      @prod_ticks += Benchmark.realtime{ actual = solve( src ) }
       assert_equal expected, actual, "src=#{src}"
     end
   end
