2e8d56c33789b03cc679274fe22099cce362fc0f
Nabetani <takenori@nabetani.sakura.ne.jp>
2019-03-02 16:43:14 +0900

    解く。

--- e31/src/solver.rb
+++ e31/src/solver.rb
@@ -1,77 +1,66 @@
 # frozen_string_literal: true
 
-DIGITS=[*"0".."9", *"a".."z"]
 
-def number(r)
-  r.map{ |e| DIGITS[e] }.join
-end
-
-def build_number(b, head, size, states)
-  r=[head]
-  (size-1).times do |ix|
-    p = r.last
-    candidates = [ p, (p+1) % b ].sort
-    r.push candidates[
-      [nil, :below, :low].include?(states[ix]) ? 0 : 1
-    ]
-  end
-  r
-end
-
-def undigits(b,d)
-  r=0
-  d.reverse.each do |n|
-    r*=b
-    r+=n
-  end
-  r
-end
-
-def solve_impl(b,digits)
-  ignore=false
-  states = digits.each_cons(2).map{ |p,q|
-    candidates = [ p, (p+1) % b ].sort
-    if ignore
-      nil
-    elsif q<candidates[0]
-      ignore=true
+def state(b, x)
+  x.each_cons(2).map{ |prev,cur|
+    lo, hi = [ prev, (prev+1) % b ].sort
+    if cur<lo
       :below
-    elsif q==candidates[0]
-      :low
-    elsif q<candidates[1]
-      ignore=true
+    elsif cur==lo
+      :lo
+    elsif cur<hi
       :mid
-    elsif q==candidates[1]
-      :high
+    elsif cur==hi
+      :hi
     else
-      ignore=true
       :above
     end
   }
-  above = states.index(:above)
-  if above
-    n = undigits(b,digits[0,above+1].reverse)
-    d = (n+1).digits(b).reverse
-    head = solve_impl(b,d)
-    num = [ head.last, (head.last+1) % b ].min
-    tail = [num] * (digits.size-above-1)
-    head+tail
-  else
-    build_number(b, digits.first, digits.size, states)
+end
+
+def count_state( s )
+  return 1 if s.empty?
+  case s[0]
+  when :below then 0
+  when :lo then count_state(s.drop(1))
+  when :mid then 2**(s.size-1)
+  when :hi then count_state(s.drop(1)) + 2**(s.size-1)
+  when :above then 2**s.size
   end
 end
 
+# x 以下の ぐるぐる数の個数
+def guru_count( b, x )
+  return x[0] if x.size==1
+  # x.size 未満の桁数
+  s0 = (1..(x.size-1)).sum{ |keta| (b-1)*(2**(keta-1)) }
+  # x.size 桁で、先頭が 1..(x.first-1)
+  s1 = (x.first-1)*(2**(x.size-1))
+  # 先頭が x.first
+  s2 = count_state( state(b, x) )
+  [s0, s1, s2].sum
+end
+
 def solve( src )
-  sb,n = src.split(",")
+  sb,sx,sy = src.split(",")
   b = sb.to_i
-  digits = (n.to_i(b)+1).digits(b).reverse
-  solve_impl(b, digits).map{ |e| DIGITS[e] }.join
+  x = (sx.to_i(b) - 1 ).digits(b).reverse
+  y = sy.to_i(b).digits(b).reverse
+  ycount = guru_count(b,y)
+  xcount = guru_count(b,x)
+  ( ycount - xcount ).to_s
 end
 
 if __FILE__==$PROGRAM_NAME
-  [ "4,30330" ].each do |src|
-    b,n = src.split(",").map(&:to_i)
-    digit = n.to_s(b)
-    puts( "%s ( %s ) -> %s" % [src, digit, solve(src)] )
+  [
+    [ "3,1,20", "5" ],
+    [ "3,1,110", "6" ],
+    [ "3,1,12", "4" ],
+    [ "3,1,120", "9" ],
+    [ "3,1,210", "12" ],
+    [ "3,1,2020", "26" ],
+  ].each do |src, expected|
+    actual = solve(src)
+    puts( "%s -> %s ( %s )" % [ src, actual, expected ] )
   end
 end
