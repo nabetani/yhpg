89eb839850731f25d90ec250c51b591d784090b6
Nabetani <takenori@nabetani.sakura.ne.jp>
2019-03-02 22:44:54 +0900

    サンプルデータの差し替えと、サンプルデータのバリデーション

--- e31/src/problem.rb
+++ e31/src/problem.rb
@@ -75,15 +75,51 @@ def candidate(b)
   [b,n.to_s(b)].join(",")
 end
 
-def rand_samples( bvalues, erange, seed )
-  rng = Random.new(seed)
-  bvalues.map{ |b|
-    e = rng.rand(erange)
-    max = 2**e
-    hi = rng.rand((max/2)..max)
-    lo = rng.rand((hi/4)..(hi/2))
-    [b,*[lo,hi].map{ |e| e.to_s(b) }].join(",")
+def makesrc(b,lo,hi)
+  [b, *[lo,hi].map{ |x| x.to_s(b) } ].join(",")
+end
+
+def find_upper(b, lo, c)
+  (1..).each{ |e|
+    hi=lo+2**e
+    src = makesrc(b,lo,hi)
+    return hi if c<solve(src).to_i
   }
+end
+
+def find_hival(b,lo,c)
+  upper = find_upper( b, lo, c )
+  ((lo+1)..upper).bsearch{ |hi|
+    src = makesrc(b,lo,hi)
+    c<=solve(src).to_i
+  }
+end
+
+def rand_samples1
+  rng = Random.new(1)
+  d = [*2..10,*2..36].zip( [*2..850].sort_by{ rng.rand } )
+  d.map{ |b,c|
+    lo = rng.rand(b**3)+1
+    hi = find_hival(b, lo, c)
+    makesrc(b,lo,hi+rand(b**2))
+  }
+end
+
+def rand_samples2
+  rng = Random.new(2)
+  Array.new(10){
+    b = rng.rand(2..36)
+    e = rng.rand(4..15)
+    hi = rng.rand(2**e..2**(e+1))
+    lo = rng.rand((hi/4)..(hi/2))
+    makesrc(b,lo,hi)
+  }
+end
+
+def zero(b,start)
+  lo = start.to_i(b)
+  hi = find_hival(b,lo,1)
+  makesrc(b,lo,hi-1)
 end
 
 SAMPLES = [
@@ -93,11 +129,28 @@ SAMPLES = [
   "36,1,z",
   "10,100,110",
   "5,3401,40123",
+  zero(36,"zoo"),
+  zero(4,"1300000"),
   "2,1010010110110110000110101,11010111001011110110110101",
   "3,22111101011101,11021122211120221",
   "4,10030022033,10203020123103",
   "10,3268665,134217728",
   "36,6pku,27wr28",
-  rand_samples( 2.step(36,3), 4..13, 1 ),
-  rand_samples( 2..36, 14..20, 2 ),
+  rand_samples1,
+  rand_samples2,
 ].flatten.sort_by{ |x| [solve(x).to_i, x.size, x] }
+
+SAMPLES.each{ |s|
+  split = s.split(",")
+  raise s unless split.size==3
+  sb, sx, sy = split
+  b = sb.to_i
+  raise s unless b.to_s==sb
+  raise s unless (2..36)===b
+  x, y = [sx, sy].map{ |e| e.to_i(b) }
+  raise s unless x.to_s(b)==sx
+  raise s unless y.to_s(b)==sy
+  raise s unless x<=y
+  raise s unless y<=2**52
+  raise s unless 0<x
+}
