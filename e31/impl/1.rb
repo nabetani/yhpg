0317ebaffeb4f96d447b14e24a582123cb78d395
Nabetani <takenori@nabetani.sakura.ne.jp>
2019-03-09 14:36:35 +0900

    リファクタリング

--- e31/impl/1.rb
+++ e31/impl/1.rb
@@ -31,16 +31,13 @@ end
 def guru(b,digits)
   states = build_states(b, digits)
   above = states.index(:above)
-  if above
-    # 繰り上がり処理がある
-    n = undigits(b,digits[0,above+1].reverse)
-    d = (n+1).digits(b).reverse
-    head = guru(b,d)
-    num = next_vals( b, head.last).min
-    head + [num] * (digits.size-above-1)
-  else
-    build_number(b, digits.first, digits.size, states)
-  end
+  return build_number(b, digits.first, digits.size, states) unless above
+  # 繰り上がり処理がある
+  n = undigits(b,digits[0,above+1].reverse)
+  d = (n+1).digits(b).reverse
+  head = guru(b,d)
+  num = next_vals( b, head.last).min
+  head + [num] * (digits.size-above-1)
 end
 
 def sup(b,x)
@@ -58,18 +55,20 @@ def solve_impl( b, x, y )
 end
 
 def solve( src )
-  sb, sx, sy = src.split(",")
+  sb, *sxy = src.split(",")
   b = sb.to_i
+  xy = sxy.map{ |e| e.to_i(b) }
   if b==2
-    (sy.to_i(2) - sx.to_i(2)+1).to_s
+    (xy[1] - xy[0]+1).to_s
   else
-    solve_impl( b, sx.to_i(b), sy.to_i(b) ).to_s
+    solve_impl( b, *xy ).to_s
   end
 end
 
 if __FILE__==$PROGRAM_NAME
-  json = JSON.parse( File.open( "../page/data.json" ){ |f| f.read }, symbolize_names:true )
-  json[:test_data].each do |number:, src:, expected:|
+  json = File.open( "../page/data.json", &:read)
+  data = JSON.parse( json, symbolize_names:true )
+  data[:test_data].each do |number:, src:, expected:|
     actual = solve( src )
     p [ (expected==actual ? "ok" : "**NG**"), number, src, expected, actual ]
   end
