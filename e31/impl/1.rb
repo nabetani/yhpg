20de4123de045bc0e3eb3100856003e3b6e9ca87
Nabetani <takenori@nabetani.sakura.ne.jp>
2019-03-09 13:26:06 +0900

    solver.rb と異なる方針での実装開始

---
+++ e31/impl/1.rb
@@ -0,0 +1,78 @@
+# frozen_string_literal: true
+
+require "json"
+
+DIGITS=[*"0".."9", *"a".."z"]
+
+def number(r)
+  r.map{ |e| DIGITS[e] }.join
+end
+
+def build_number(b, head, size, states)
+  r=[head]
+  (size-1).times do |ix|
+    p = r.last
+    candidates = [ p, (p+1) % b ].sort
+    r.push candidates[
+      [nil, :below, :low].include?(states[ix]) ? 0 : 1
+    ]
+  end
+  r
+end
+
+def undigits(b,d)
+  r=0
+  d.reverse.each do |n|
+    r*=b
+    r+=n
+  end
+  r
+end
+
+def guru(b,digits)
+  ignore=false
+  states = digits.each_cons(2).map{ |p,q|
+    candidates = [ p, (p+1) % b ].sort
+    if ignore
+      nil
+    elsif q<candidates[0]
+      ignore=true
+      :below
+    elsif q==candidates[0]
+      :low
+    elsif q<candidates[1]
+      ignore=true
+      :mid
+    elsif q==candidates[1]
+      :high
+    else
+      ignore=true
+      :above
+    end
+  }
+  above = states.index(:above)
+  if above
+    n = undigits(b,digits[0,above+1].reverse)
+    d = (n+1).digits(b).reverse
+    head = guru(b,d)
+    num = [ head.last, (head.last+1) % b ].min
+    tail = [num] * (digits.size-above-1)
+    head+tail
+  else
+    build_number(b, digits.first, digits.size, states)
+  end
+end
+
+def solve( src )
+  b,n = src.split(",").map(&:to_i)
+  digits = (n+1).digits(b).reverse
+  guru(b, digits).map{ |e| DIGITS[e] }.join
+end
+
+if __FILE__==$PROGRAM_NAME
+  json = JSON.parse( File.open( "../page/data.json" ){ |f| f.read }, symbolize_names:true )
+  json[:test_data].each do |number:, src:, expected:|
+    actual = solve( src )
+    p [ number, src, expected, actual ]
+  end
+end
