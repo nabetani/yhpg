41401cc795bad15f96ac0c4987c73b98da022f33
Nabetani <takenori@nabetani.sakura.ne.jp>
2019-03-17 12:22:31 +0900

    矩形を表示する

--- e32/src/problem.rb
+++ e32/src/problem.rb
@@ -7,26 +7,12 @@ EVENT_URL="https://yhpg.doorkeeper.jp/events/88379"
 QIITA_URL = nil
 TITLE="きれいな四角 #{EVENT_DATE.join(".")}"
 
-S0="11,22,33,44/55,66,77,88/20,30,99,99"
 
-Rect = Struct.new( :x, :y, :right, :bottom ) do
-  def initialize( *src )
-    case src.size
-    when 1
-      super( *src[0].split(",").map(&:to_i) )
-    when 4
-      super(*src)
-    end
-  end
-
-  def w
-    right-x
-  end
-
-  def h
-    bottom-y
-  end
+def randcol
+  "#%03x" % rand(0x1000)
 end
+
+S0="1ayd/b3gw/d7qq"
 
 SAMPLES = [
   S0
