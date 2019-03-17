813453d1c47a2634c29922ce664056bc16bb52ce
Nabetani <takenori@nabetani.sakura.ne.jp>
2019-03-17 12:22:11 +0900

    テストデータを追加する

--- e32/src/solver_test.rb
+++ e32/src/solver_test.rb
@@ -4,6 +4,7 @@ require_relative "solver"
 class SolverTest < Minitest::Test
   def test_samples
     [
+      [ "1ayd/b3gw/d7qq", "TBD" ],
     ].each do |src, expected|
       actual = solve( src )
       assert_equal expected, actual, "src=#{src}"
