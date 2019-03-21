01003c416517f2b69cf25050afeb17895ba6c06f
Nabetani <takenori@nabetani.sakura.ne.jp>
2019-03-21 16:26:06 +0900

    リファクタリング：HSeg  と YSeg の区別をやめる

--- e32/src/solver.rb
+++ e32/src/solver.rb
@@ -5,9 +5,7 @@ require "pp"
 FIELD_H = 36
 FIELD_W = 36
 
-HSeg = Struct.new( :y, :left, :right )
-
-VSeg = Struct.new( :x, :top, :bottom )
+Seg = Struct.new( :level, :lo, :hi )
 
 Rect = Struct.new( :x, :y, :right, :bottom ) do
   def initialize( *src )
@@ -22,11 +20,11 @@ Rect = Struct.new( :x, :y, :right, :bottom ) do
   end
 
   def hsegs
-    [HSeg.new( y, x, right ), HSeg.new( bottom, x, right ) ]
+    [Seg.new( y, x, right ), Seg.new( bottom, x, right ) ]
   end
 
   def vsegs
-    [VSeg.new( x, y, bottom), VSeg.new( right, y, bottom ) ]
+    [Seg.new( x, y, bottom), Seg.new( right, y, bottom ) ]
   end
 
   def w
@@ -61,36 +59,26 @@ class Solver
     @vsegs = rects.flat_map(&:vsegs).sort_by(&:values)
   end
 
-  def hseg_cover?(s)
-    non_covered = s.left
-    @hsegs.select{ |e| e.y == s.y }.each do |e|
-      if e.left<=non_covered && non_covered<e.right
-        non_covered = e.right
+  def seg_cover?(s, segs)
+    non_covered = s.lo
+    segs.select{ |e| e.level == s.level }.each do |e|
+      if e.lo<=non_covered && non_covered<e.hi
+        non_covered = e.hi
       end
     end
-    s.right<=non_covered
-  end
-
-  def vseg_cover?(s)
-    non_covered = s.top
-    @vsegs.select{ |e| e.x == s.x }.each do |e|
-      if e.top<=non_covered && non_covered<e.bottom
-        non_covered = e.bottom
-      end
-    end
-    s.bottom<=non_covered
+    s.hi<=non_covered
   end
 
   def clean?(c)
     return false if @rects.any?{ |rc| c.partial_intersect?(rc) }
-    return false unless c.hsegs.all?{ |s| hseg_cover?(s) }
-    return false unless c.vsegs.all?{ |s| vseg_cover?(s) }
+    return false unless c.hsegs.all?{ |s| seg_cover?(s, @hsegs) }
+    return false unless c.vsegs.all?{ |s| seg_cover?(s, @vsegs) }
     true
   end
 
   def cleans
-    ys = @hsegs.map(&:y).sort.uniq
-    xs = @vsegs.map(&:x).sort.uniq
+    ys = @hsegs.map(&:level).sort.uniq
+    xs = @vsegs.map(&:level).sort.uniq
     ys.combination(2).flat_map do |t,b|
       xs.combination(2).flat_map do |l,r|
         c = Rect.new( l, t, r, b )
