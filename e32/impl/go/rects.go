db64112091e834898d3c010c4378a6e324f38dbf
Nabetani <takenori@nabetani.sakura.ne.jp>
2019-04-06 17:38:03 +0900

    解いた。

--- e32/impl/go/rects.go
+++ e32/impl/go/rects.go
@@ -11,6 +11,9 @@ import (
 	"strings"
 )
 
+const n = 0x1000
+const wh = 36
+
 type rectType struct {
 	x, y, r, b int
 }
@@ -36,8 +39,101 @@ func createRect(s string) rectType {
 	}
 }
 
+func makeField(rects []rectType) map[int]int {
+	f := map[int]int{}
+	for ix, r := range rects {
+		for y := r.y; y < r.b; y++ {
+			for x := r.x; x < r.r; x++ {
+				f[y*n+x] += (1 << uint(ix))
+			}
+		}
+	}
+	return f
+}
+
+func findRight(field map[int]int, x0, y0 int) int {
+	val0 := field[x0+y0*n]
+	for x := x0; x < wh+1; x++ {
+		if val0 != field[x+y0*n] {
+			return x
+		}
+	}
+	return -1
+}
+
+func findBottom(field map[int]int, x0, y0 int) int {
+	val0 := field[x0+y0*n]
+	for y := y0; y < wh+1; y++ {
+		if val0 != field[x0+y*n] {
+			return y
+		}
+	}
+	return -1
+}
+
+func hborder(field map[int]int, x0, x1, y0, y1 int) bool {
+	val := field[y0*n+x0]
+	for x := x0; x < x1; x++ {
+		if field[y0*n+x] != val || field[y1*n+x] == val {
+			return false
+		}
+	}
+	return true
+}
+
+func vborder(field map[int]int, y0, y1, x0, x1 int) bool {
+	val := field[y0*n+x0]
+	for y := y0; y < y1; y++ {
+		if field[y*n+x0] != val || field[y*n+x1] == val {
+			return false
+		}
+	}
+	return true
+}
+func samecol(field map[int]int, x0, y0, r, b int) bool {
+	col := field[y0*n+x0]
+	for y := y0; y < b; y++ {
+		for x := x0; x < r; x++ {
+			if col != field[y*n+x] {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func isCleanRect(field map[int]int, x, y, r, b int) bool {
+	if r <= x || b <= y {
+		return false
+	}
+	return hborder(field, x, r, y, y-1) &&
+		hborder(field, x, r, b-1, b) &&
+		vborder(field, y, b, x, x-1) &&
+		vborder(field, y, b, r-1, r) &&
+		samecol(field, x, y, r, b)
+}
+
+func getRect(field map[int]int, x, y int) *rectType {
+	r := findRight(field, x, y)
+	b := findBottom(field, x, y)
+	if isCleanRect(field, x, y, r, b) {
+		return &rectType{x, y, r, b}
+	}
+	return nil
+}
+
 func solveImpl(rects []rectType) []rectType {
-	return rects
+	fields := makeField(rects)
+	res := []rectType{}
+	for y := 0; y < wh; y++ {
+		for x := 0; x < wh; x++ {
+			rect := getRect(fields, x, y)
+			if rect != nil {
+				res = append(res, *rect)
+			}
+		}
+	}
+	return res
 }
 
 func solve(src string) string {
